1048fa4c5ea47464c188d1d2ef8f6edebb76366b
Jhon Jairo Hernandez Pulgarin <jhon@appenate.com>
2024-02-29 18:31:35 -0500

    Se crea IProducto y el servicio producto

---
+++ front/compras/src/app/data/IProducto.ts
@@ -0,0 +1,13 @@
+export interface IProducto {
+    IdProducto: string;
+    Nombre: string,
+    Referencia: string,
+    UrlImagen: string, 
+    Descripcion: string,
+    Color: string,
+    Cantidad: number,
+    Talla: string,
+    Valor: number,
+    EsDeLosMasBuscados: boolean,
+    ParaSexo: string
+}
---
+++ front/compras/src/app/data/producto.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { IProducto } from './IProducto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+
+  constructor(private http: HttpClient) { }
+
+  GetByName(nombre: string): Observable<any> {  
+    console.log("Antes de consultar Producto GetByName, el nombre del producto es " + nombre);   
+    return this.http.get('https://localhost:7145/Producto/GetByName'+  "/" + nombre);
+  }
+  
+  GetMasBuscados(paraSexo: string): Observable<any> {  
+    console.log("Antes de consultar Producto GetByName, el nombre del producto es " + paraSexo);   
+    return this.http.get('https://localhost:7145/Producto/GetMasBuscados'+  "/" + paraSexo);
+  }
+}
