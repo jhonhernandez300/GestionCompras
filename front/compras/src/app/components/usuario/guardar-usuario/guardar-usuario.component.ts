d2433c7b9d7bbf2c36578336db278ae954ceb6f7
Jhon Jairo Hernandez Pulgarin <jhon@appenate.com>
2024-03-06 15:18:04 -0500

    se pone mensaje de error en el correo q ya existe

--- front/compras/src/app/components/usuario/guardar-usuario/guardar-usuario.component.ts
+++ front/compras/src/app/components/usuario/guardar-usuario/guardar-usuario.component.ts
@@ -89,26 +89,21 @@ export class GuardarUsuarioComponent implements OnInit {
       console.error(': ', error);
     })                    
   }  
-
-  // Función para verificar si el correo electrónico ya existe
+  
   checkEmailExists(email: string): Observable<any> {
     return this.usuarioService.CheckEmail(email);
   }
-
-  // Controlador de eventos para el evento de enfoque en el campo de contraseña
+  
   onPasswordFocus() {
     const email = this.myForm.get('CorreoElectronico')?.value;
     if (email) {
       this.checkEmailExists(email).subscribe(
-        (response) => {
-          // Maneja la respuesta del servicio aquí
-          console.log('Correo electrónico ya existe:', response);
-          // Puedes mostrar un mensaje de error apropiado aquí
+        (response) => {                    
+          console.log('Correo electrónico ya existe:', response);          
+          this.myForm.get('CorreoElectronico')?.setErrors({ 'correoExiste': true });
         },
-        (error) => {
-          // Maneja el error aquí
-          console.error('Error al verificar el correo electrónico:', error);
-          // Puedes mostrar un mensaje de error apropiado aquí
+        (error) => {          
+          console.error('Error al verificar el correo electrónico:', error);          
         }
       );
     }
